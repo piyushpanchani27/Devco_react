2c748a3ec8d985c97f7059bfc36e435d531f485c
langton <langtonmwanza@gmail.com>
2025-01-02 19:07:03 +0200

    Adding Date Badge

--- components/auction/AuctionCard.tsx
+++ components/auction/AuctionCard.tsx
@@ -1,36 +1,14 @@
-import {Badge, Center, Flex, Grid, Paper, Stack, Text} from "@mantine/core";
+import {Badge, Flex, Grid, Paper, Stack, Text} from "@mantine/core";
 import CountdownTimer from "../common/CountdownTimer";
 import {Auction} from "../../lib/types";
 import dayjs, {Dayjs} from "dayjs";
+import DateBadge from "../common/DateBadge";
+
 
 export default function AuctionCard({auction}: { auction: Auction }) {
     const getDateLabel = (date: Dayjs, label: string, badgeColor: string) => {
         return (
-            <Paper p={5} h={60} shadow={'none'} withBorder radius={0}>
-                <Grid>
-                    <Grid.Col span={5}>
-                        <Center>
-                            <Badge size={'14'} w={65} h={48} radius={0}
-                                   color={badgeColor}>{date.format('DD')}</Badge>
-                            <Badge size={'12'} w={70} h={48} radius={0}
-                                   color="gray.4">
-                                <Stack>
-                                    <Text c={'gray.7'} fw={800}
-                                          fz={15}>{date.format('MMM')}</Text>
-                                    <Text c={'gray.7'} mt={-20} fw={600}
-                                          fz={12}>{date.format('YYYY')}</Text>
-                                </Stack>
-                            </Badge>
-                        </Center>
-
-                    </Grid.Col>
-                    <Grid.Col span={7}>
-                        <Center h={50}>
-                            <Text fw={800} fz={15} c={'gray.6'}>{label}</Text>
-                        </Center>
-                    </Grid.Col>
-                </Grid>
-            </Paper>)
+            <DateBadge color={badgeColor} date={date} label={label}/>)
     }
 
     return (
