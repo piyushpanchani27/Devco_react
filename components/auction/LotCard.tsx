93fac93f0476d69df6ed0fd71bcb4fd60cf87933
langton <langtonmwanza@gmail.com>
2025-01-03 14:47:41 +0200

    Adding lots page

---
+++ components/auction/LotCard.tsx
@@ -0,0 +1,53 @@
+import {Lot} from "../../lib/types";
+import {ActionIcon, Badge, Box, Button, Center, Flex, Group, Image, Paper, Stack, Text} from "@mantine/core";
+import {IconStar} from "@tabler/icons-react";
+import CountdownTimer from "../common/CountdownTimer";
+import dayjs from "dayjs";
+
+export default function LotCard({lot}: { lot: Lot }) {
+    return (
+        <Paper withBorder>
+            <Group>
+                <Box w={60} h={200} bg={'primary.9'}>
+                    <Stack>
+                        <Flex justify={"center"}>
+                            <Text fw={600} c={'white'}>LOT</Text>
+                        </Flex>
+                        <Center h={140}>
+                            <Text fw={600} c={'white'}>{lot.lotnumber}</Text>
+                        </Center>
+                    </Stack>
+                </Box>
+                <Center w={200}>
+                    <Image mt={-20} w={200} src={lot.image[0]} alt={lot.title}/>
+                </Center>
+
+                <Stack w={200} p={10} h={200}>
+                    <Group>
+                        <ActionIcon variant="transparent" aria-label="Settings">
+                            <IconStar style={{width: '70%', height: '70%'}} stroke={1.5}/>
+                        </ActionIcon>
+                        <Text fw={700} fz={17} c={'primary.9'}>
+                            {lot.title}
+                        </Text>
+                    </Group>
+                    <Group>
+                        <Text fw={800} fz={15}>Year :</Text>
+                        <Text fw={600} fz={15}>{lot.year}</Text>
+                    </Group>
+                    <Button>
+                        Place Bid
+                    </Button>
+
+                </Stack>
+                <Stack w={300} h={200}>
+                    <Center h={150}>
+                        <CountdownTimer timestamp={dayjs(lot.end_time).unix() * 1000} completedComponent={<>Closed</>}/>
+                    </Center>
+                    <Badge bg={'gray'} radius={0} size={"lg"} w={'100%'}>Starting Bid | R {lot.startingprice}</Badge>
+                </Stack>
+            </Group>
+        </Paper>
+
+    )
+}
--- pages/UpcomingAuctions/[id]/Lots/index.tsx
+++ pages/UpcomingAuctions/[id]/Lots/index.tsx
@@ -6,19 +6,22 @@ import {useRouter} from "next/router";
 import {liveAuctionsAtom} from "../../../../src/atoms/auctionAtoms";
 import LotsPageHeader from "../../../../components/auction/LotsPageHeader";
 import LotsFilterBar from "../../../../components/auction/LotsFilterBar";
+import {lotsByAuctionAtom} from "../../../../src/atoms/lotsAtoms";
+import LotCard from "../../../../components/auction/LotCard";
 
 export default function LotsPage() {
     const [, setPageTitle] = useAtom(pageTitleAtom)
     const [auctions,] = useAtom(liveAuctionsAtom)
+    const [auctionLots,] = useAtom(lotsByAuctionAtom)
     const router = useRouter();
     const {id} = router.query;
-
-    const auction = auctions.find(auction => auction?.id === parseInt(id?.toString() ?? ''))
-    console.log(auction)
 
     useEffect(() => {
         setPageTitle('Lots')
     })
+
+    const auction = auctions.find(auction => auction?.id === parseInt(id?.toString() ?? ''))
+    const lots = auctionLots(auction?.id ?? 0)
 
     if (!auction) return <></>
 
@@ -30,7 +33,12 @@ export default function LotsPage() {
                     <Grid.Col span={3}>
                         <LotsFilterBar auction={auction}/>
                     </Grid.Col>
-                    <Grid.Col span={9}></Grid.Col>
+                    <Grid.Col span={9}>
+                        {
+                            lots.map((lot, index) =>
+                                <LotCard lot={lot} key={index}/>)
+                        }
+                    </Grid.Col>
                 </Grid>
             </Stack>
         </Center>
